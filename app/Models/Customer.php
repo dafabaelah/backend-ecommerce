daae3d9137aee452755d204a4e275fb0b6bc4f38
dafabaelah <dafabaelah12@gmail.com>
2024-06-24 16:21:29 +0700

    seting database pada .env & buat database menggunakan ORM

---
+++ app/Models/Customer.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+// membuat model Customer dengan perintah php artisan make:model Customer -m
+// flag "-m" untuk membuat migration
+
+class Customer extends Model
+{
+    use HasFactory;
+    
+    /**
+     * fillable
+     *
+     * @var array
+     */
+
+    // fillable adalah properti yang digunakan untuk menentukan field mana saja yang boleh diisi
+    
+    protected $fillable = [
+        'name', 'email', 'email', 'email_verified_at', 'password', 'remember_token'
+    ];
+}
